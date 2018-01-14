b816d4d540409253b48715db8775218c50eaa7f2
Mayuresh Rao <mayureshvrao@gmail.com>
2018-01-14 13:38:45 -0800

    alert added

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func showAlert(_ sender: Any) {
+        let alert = UIAlertController.init(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction.init(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
