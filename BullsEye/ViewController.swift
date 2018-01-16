c0db53f2600803056b7c38990fef190590d6e539
Mayuresh Rao <mayureshvrao@gmail.com>
2018-01-16 00:02:13 -0800

    Bonus logic added

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider : UISlider!
     var targetValue : Int = 0
     var score  = 0
+    var round = 1
+    var bonus = 50
     @IBOutlet weak var targetValueOutlet: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         currentValue = lroundf(slider.value)
@@ -23,12 +26,14 @@ class ViewController: UIViewController {
     func startNewRound (){
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue =  50
+        round += 1
         slider.value = Float(currentValue)
         updateLabels()
     }
     func updateLabels (){
         targetValueOutlet.text = String(targetValue)
         scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -36,10 +41,24 @@ class ViewController: UIViewController {
     }
     @IBAction func showAlert(_ sender: Any) {
         let  difference = abs(currentValue - targetValue)
-        let points = 100 - difference
+        var points = 100 - difference
+        let title :String
+        if difference == 0{
+            title = "Perfect!"
+            points += 100
+        }else if difference < 5{
+            title = "You almost had it!"
+            if difference == 1{
+            points += 50
+            }
+        }else if difference < 10{
+            title = "Pretty good!"
+        }else{
+            title = "Not even close..."
+        }
         score += points
-        let message = "You scored \(score) points"
-        let alert = UIAlertController.init(title: "Hello, World!", message: message, preferredStyle: .alert)
+        let message = "You scored \(points) points"
+        let alert = UIAlertController.init(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction.init(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
